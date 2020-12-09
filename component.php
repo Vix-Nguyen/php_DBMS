14830164addb15a09b201dbf2d430c4ab852c627
Vix-Nguyen <59189226+Vix-Nguyen@users.noreply.github.com>
2020-12-10 00:23:13 +0700

    update display patient

--- component.php
+++ component.php
@@ -1,19 +1,78 @@
 <?php
 
-function searchPatient($id, $fname, $lname, $DoB, $Phone, $Addr, $gender){
+function searchPatient($id, $fname, $lname, $Phone){
+
     $element = "
     <tr>
         <td>$id</td>
         <td>$fname</td>
         <td>$lname</td>
-        <td>$DoB</td>
         <td>$Phone</td>
-        <td>$Addr</td>
-        <td>$gender</td>
     </tr>
     ";
 
     echo $element;
+    getInfor($id);
+}
+
+function getInfor($id){
+    require("connection.php");
+
+    $class = substr($id, 0 ,2);
+    $element = "";
+    if ($class == "IP") {
+        $element ="    
+        <tr style='background-color: tomato'>
+            <td>Doctor</td>
+            <td>Start Date</td>
+            <td>End Date</td>
+            <td>Result</td>
+        </tr>";
+        echo $element;
+        $sql = "SELECT * FROM treatment WHERE PatientId='$id'";
+        $result = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_assoc($result)){
+            $docID = $row['DoctorId'];
+            $stD = $row['StartDate'];
+            $enD = $row['EndDate'];
+            $rsl = $row['Result'];
+            $element= "
+            <tr style='background-color: wheat'>
+                <td>$docID</td>
+                <td>$stD</td>
+                <td>$enD</td>
+                <td>$rsl</td>
+            </tr>";
+            echo $element;
+        }
+    }
+    else{
+        $element ="    
+        <tr style='background-color: tomato'>
+            <td>Doctor</td>
+            <td>Examination Date</td>
+            <td>Fee</td>
+            <td>Second Date</td>
+        </tr>";
+        echo $element;
+
+        $sql = "SELECT * FROM examination WHERE PatientId='$id'";
+        $result = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_assoc($result)){
+            $docID = $row['DoctorId'];
+            $stD = $row['ExamDate'];
+            $enD = $row['Fee'];
+            $rsl = $row['SecondDate'];
+            $element="
+            <tr style='background-color: wheat'>
+                <td>$docID</td>
+                <td>$stD</td>
+                <td>$enD</td>
+                <td>$rsl</td>
+            </tr>";
+            echo $element;
+        }
+    }
 }
 
 function printInpatientByDortor($fname, $lname, $phone, $gender, $result){
